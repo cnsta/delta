9bc511298b7dd9d8cb2cc0833df481ea17446b03
cnst <adam@cnst.dev>
2026-08-26 11:05:07 +0200

    first draft build

---
+++ build.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const scanner = @import("wayland").Scanner.create(b, .{});
+    scanner.addCustomProtocol(b.path("protocol/river-window-management-v1.xml"));
+    scanner.addCustomProtocol(b.path("protocol/river-xkb-bindings-v1.xml"));
+    scanner.generate("river_window_manager_v1", 4);
+    scanner.generate("river_xkb_bindings_v1", 3);
+    const wayland = b.createModule(.{ .root_source_file = scanner.result });
+
+    const xkbcommon = b.dependency("xkbcommon", .{}).module("xkbcommon");
+
+    const files = b.addWriteFiles();
+    const headers = files.add("headers.h",
+        \\#include <linux/input-event-codes.h>
+    );
+    const input_event_codes = b.addTranslateC(.{
+        .root_source_file = headers,
+        .optimize = optimize,
+        .target = target,
+        .link_libc = true,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "delta",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "wayland", .module = wayland },
+                .{ .name = "xkbcommon", .module = xkbcommon },
+                .{ .name = "event-codes", .module = input_event_codes.createModule() },
+            },
+        }),
+    });
+
+    exe.root_module.linkSystemLibrary("wayland-client", .{});
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run delta");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const test_step = b.step("test", "Run unit tests");
+    const tests = b.addTest(.{ .root_module = exe.root_module });
+    test_step.dependOn(&b.addRunArtifact(tests).step);
+}
