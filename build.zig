1049e3a7e24b1c5da22ccf4f461f2f0717fd6130
cnst <adam@cnst.dev>
2026-08-27 11:16:36 +0200

    change name, so not to clash with git delta

--- build.zig
+++ build.zig
@@ -27,7 +27,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "delta",
+        .name = "delta-wm",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
