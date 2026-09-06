cc2d24f7ac60e3b443a256fbcde74a2e3a8cddf9
cnst <adam@cnst.dev>
2026-09-06 19:08:43 +0200

    window/ws: simplify apply tail, maybe

--- src/Window.zig
+++ src/Window.zig
@@ -376,23 +376,10 @@ fn apply(window: *Window, p: rules.Placement) void {
         });
     }
 
-    if (window.placed == null) {
-        if (window.workspace) |ws| ws.last_spawn_time = wm.millis();
-        window.placeInSlot(.immediate);
-    } else if (size_changed) {
+    if (window.placed == null or size_changed) {
         window.placeInSlot(.immediate);
     } else if (origin_moved) {
-        const duration = if (wm.config.animation.enabled) wm.config.animation.duration_ms else 0;
-        const spawn_recent = if (window.workspace) |ws|
-            (wm.millis() - ws.last_spawn_time < duration)
-        else
-            false;
-
-        if (spawn_recent) {
-            window.placeInSlot(.immediate);
-        } else {
-            window.placeInSlot(.animated);
-        }
+        window.placeInSlot(.animated);
     }
 }
 
